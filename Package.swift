57070af3d4d7a0e4b273a0a4577e631f99460bfb
AVVS <v@makeomatic.ca>
2020-01-05 14:17:54 -0800

    feat: use Package.swift

---
+++ Package.swift
@@ -0,0 +1,16 @@
+// swift-tools-version:5.1
+import PackageDescription
+
+let package = Package(
+  name: "Animo",
+  platforms: [
+    .iOS(.v11), .tvOS(.v11)
+  ],
+  products: [
+    .library(name: "Animo", targets: ["Animo"])
+  ],
+  targets: [
+    .target(name: "Animo", dependencies: [], path: "Animo/")
+  ],
+  swiftLanguageVersions: [.version("5")]
+)
